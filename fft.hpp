d5477768e667b0dc809f68950ddd37f9d3560e11
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2024-01-01 16:33:47 +0300

    Update fft.hpp

--- fft.hpp
+++ fft.hpp
@@ -6,6 +6,8 @@ class fft_class {
 public:
     fft_class() {}
 
+    static constexpr double eps = 1e-5;
+
     static std::vector<complex> fft_inner(const std::vector<complex>& poly, complex root) {
         std::size_t n = poly.size();
         if (n == 1) {
@@ -28,34 +30,30 @@ public:
         return res;
     }
 
-    static std::vector<complex> fft(const std::vector<int64_t>& poly) {
+    static std::vector<complex> fft(std::vector<complex> poly) {
         std::size_t n = 1;
         while (n < poly.size()) {
             n <<= 1;
         }
-        std::vector<complex> prepared_poly(n);
-        for (std::size_t i = 0; i < poly.size(); ++i) {
-            prepared_poly[i] = complex(poly[i], 0);
-        }
+        poly.resize(n);
         complex root = complex(std::cos(2 * M_PI / n), std::sin(2 * M_PI / n));
 
-        auto res = fft_inner(prepared_poly, root);
+        auto res = fft_inner(poly, root);
         return res;
     }
 
-    static std::vector<int64_t> fft_i(const std::vector<complex>& poly) {
+    static std::vector<complex> fft_i(const std::vector<complex>& poly) {
         std::size_t n = 1;
         while (n < poly.size()) {
             n <<= 1;
         }
         complex root = complex(std::cos(2 * M_PI / n), -std::sin(2 * M_PI / n));
 
-        auto complex_res = fft_inner(poly, root);
-        std::vector<int64_t> res(n);
-        for (std::size_t i = 0; i < n; ++i) {
-            res[i] = static_cast<int64_t>(std::round(complex_res[i].get_real() / n));
+        auto res = fft_inner(poly, root);
+        for (std::size_t i = 0; i < res.size(); ++i) {
+            res[i] = res[i] / n;
         }
-        while (res.back() == 0) {
+        while (res.size() && res.back().abs() < eps) {
             res.pop_back();
         }
         return res;
@@ -67,12 +65,23 @@ std::vector<int64_t> fft_multiplication(std::vector<int64_t> a,
     std::size_t n = a.size() + b.size() - 1;
     a.resize(n);
     b.resize(n);
-    auto a_fft = fft_class::fft(a);
-    auto b_fft = fft_class::fft(b);
-    assert(a_fft.size() == b_fft.size());
+    std::vector<complex> complex_a(n);
+    std::vector<complex> complex_b(n);
+    for (std::size_t i = 0; i < n; ++i) {
+        complex_a[i] = complex(a[i], 0);
+        complex_b[i] = complex(b[i], 0);
+    }
+
+    auto a_fft = fft_class::fft(complex_a);
+    auto b_fft = fft_class::fft(complex_b);
     for (std::size_t i = 0; i < a_fft.size(); ++i) {
         a_fft[i] *= b_fft[i];
     }
-    auto res = fft_class::fft_i(a_fft);
+    auto complex_res = fft_class::fft_i(a_fft);
+
+    std::vector<int64_t> res(n);
+    for (std::size_t i = 0; i < n; ++i) {
+        res[i] = static_cast<int64_t>(std::round(complex_res[i].get_real()));
+    }
     return res;
 }
